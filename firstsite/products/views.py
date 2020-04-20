420ce84345b3c9d4426a4e4da0d54071628c6f24
Adrian Eu Kai Chwen (DIGITAL/PETH) <adrian.eukaichwen@PETRONAS.PETRONET.DIR>
2020-04-21 00:28:45 +0800

    second commit

--- firstsite/products/views.py
+++ firstsite/products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    template = 'home.html'
-    context = locals()
+def home_view(request):
+    username_is = request.user
+    context = {"username": username_is}
+
+    template = 'products/home.html'
     return render(request, template, context)
